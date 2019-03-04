5e6a3b7c516bdadde57f9a9fea6b1978acb88d69
krypt0n <maxdodon25@gmail.com>
2019-03-04 11:36:44 +0200

    get POST response from server

--- main/views.py
+++ main/views.py
@@ -3,29 +3,53 @@ from django.template.context import RequestContext
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login,logout,authenticate
 from main.forms import UserForm
-import requests
+import urllib.request,json,requests
 
 def homepage(request):
     return render(request,'home.htm')
 
+
 #will send to registration page,or will send registration request to server
 def register(request):
 	if request.method == "POST":
+		#data from fields
+		email = str(request.POST.get('email'))
+		name = str(request.POST.get('username'))
+		password = str(request.POST.get('password'))
 		#json that will be send to server for registration procedure
 		jsonRequest = {
 			#put all data inside
-			"email": str(request.POST.get('email')),
-			"name" : str(request.POST.get('username')),
-			"password" : str(request.POST.get('password'))
+			"email": email,
+			"name" : name,
+			"password" : password
 		}
-		#send POST requrest to server
+		#send registration requrest to server
 		try:
-			r = requests.post('http://192.168.0.14:3000/register',jsonRequest)
+			r = requests.post('http://localhost:3000/register',jsonRequest)
+			#server response after request
+			response = str(r) + " " + r.text
+			# if response == "Account already exist":
+				
+			# else :
+			# 	login(request,name,email,password)
 			return redirect('main:homepage')
 		except Exception as e:
 			print(e)
-		
-	#send to registration page
-	else:
-	    return render(request,'registration.htm')
-	
+	#send back to registration page
+	return render(request,'registration.htm')
+
+
+
+def login(request,name,email,password):
+	#json that will be send to server for login procedure
+	jsonRequest = {
+	    "email" : str(request.POST.get('email')),
+	    "password": str(request.POST.get('password'))
+	}
+	#send login request to server
+	try:
+		r = requests.post('http://192.168.0.14:3000/login',jsonRequest)
+		return redirect('main:homepage')
+	except Exception as e:
+		print(e)
+	return redirect('main:register')
