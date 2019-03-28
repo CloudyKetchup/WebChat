d115bb3c5195422fa68de3335aeff4933f54803d
krypton17 <maxdodon25@gmail.com>
2019-03-28 13:48:52 +0200

    implemented chat rooms

--- main/static/main/javascript/index.js
+++ main/static/main/javascript/index.js
@@ -1,117 +1,106 @@
-let rect = 0;
-let roomsSpacing = 100;
-let roomsList = [];
-let room;
-var members = [];
+let rect;               // used for render of new message lower of previous
+let members = [];       // members list in new room dialog
+let selectedRoom = '';  // room id of selected room from side panel
 const roomDialog = document.getElementById('create-room-dialog');
 const roomNameField = document.querySelector('#room-name-field');
 const errorField = document.getElementById('field-error');
 
-if (isEmptyObject(rooms)) {
-    const emptyList = document.createElement("p");
-}else{
-    //get all rooms from rooms json
-    for (room in rooms) {
-        roomsList.push(room);
-    }
-    //generate rooms selectors for side menu
-    for (room in roomsList) {
-        room = document.createElement("button");
-        room.setAttribute("class","room-button");
-        room.setAttribute("name", type);
-        room.innerHTML = room['name'].toString();
-
-        let sideMenu = document.getElementById("rooms-container");
-        sideMenu.appendChild(room);
-        room.style.top = roomsSpacing+"px";
-        roomsSpacing += 50;
-    }
-}
+// handle incoming socket message
 chatSocket.onmessage = (e) => {
     data = JSON.parse(e.data);
-    response = data['response'];
-    responseHandler(response);
+    // handler
+    responseHandler(data);
 };
 
-chatSocket.onclose = (e) => {
-    console.error("socket closed");
-};
+chatSocket.onopen = () => 
+    getRooms();
 
-chatSocket.onopen = (e) => {
-    chatSocket.send(JSON.stringify({
-        'command': 'get_rooms',
-        'email': email 
-    }));
-};
+
+chatSocket.onclose = (e) => 
+    console.error("socket closed" + e);
+
 
 document.querySelector('#chat-message-input').onkeyup = (e) => {
-    if (e.keyCode === 13) {  // enter, return
+    if (e.keyCode === 13) { // enter, return
         document.querySelector('#chat-message-submit').click();
     }
 };
 
-roomNameField.onkeyup = function(){
+roomNameField.onkeyup = () => 
     errorField.style.display = "none";
-}
 
-document.querySelector('#chat-message-submit').onclick = (e) => {
-    let input = document.querySelector('#chat-message-input');
-    let message = input.value;
+// send message to socket
+document.querySelector('#chat-message-submit').onclick = () => {
+    let message = document.querySelector('#chat-message-input').value;
     chatSocket.send(JSON.stringify({
         'command': 'new_message',
         'message': message,
-        'from': username
+        'from': username,
+        'roomID': selectedRoom
     }));
-    input.value = '';
+    // empty message field when message sended
+    message = '';
 };
 
 document.getElementById("create-room").onclick = () => {
+    // spawn dialog for new room
     roomDialog.style.display = "block";
 };
 
-function responseHandler(response){
-    switch (response) {
-        case 'Member exist':
-            memberToList(data);
-            break;
-        case 'User with that name does not exist':
-            roomFormError(response);
-            break;
-        case 'Room already exist':
-            roomFormError(response);
-            break;
-        default:
-            var message = data['message'];
-            renderMessage(
-                message['content'],
-                message['author']
-            )
-            break;
-    }
-}
-
+// show error in new room dialog
 function roomFormError(response){
-    const error = document.getElementById('field-error');
-    error.style.display = "block";
-    error.innerHTML = response; 
+    const error = $('#field-error');
+    error.css('display','block');
+    error.html(response); 
 }
 
-function memberToList(data){
-    if (data['name'] != username && !members.includes(data['email'])) {
-        members.push(data['email']);
-        document.getElementById('members').innerHTML = "Members : " + members.length;
-        document.getElementById('user-email-field').value = '';
-    }else {
-        roomFormError("You can't add yourself");
+// render list of rooms in side panel 
+function renderRoomsList(response){
+    for (let i = 0; i < response['rooms'].length; i++) {
+        let roomButton = document.createElement("button");
+        // room from response
+        const room = response['rooms'][i];
+        // add room button to side panel
+        $('#rooms-container').append(roomButton);
+
+        // set style
+        roomButton.setAttribute("class","room");
+        roomButton.onclick = () => showRoom(room,response);
+        // set room name as button text
+        roomButton.innerHTML = response['rooms'][i]['name'];
     }
 }
+// show room when choosen from side panel
+function showRoom(room,response){
+    // selected room ID
+    selectedRoom = String(room['_id']);
+    // change top distance of messages to default
+    rect = 5;
+    // empty room if another was opened before
+    $('#chat-box').empty();
+    // spawn room elements
+    $('#chat-message-submit,#message-input-container,#chat-bar')
+        .css('display','block');
+    // set title to room name
+    $('#chat-title').html(
+        room['name']
+    );
+    // get room messages from database
+    fetchMessages(selectedRoom);
+}
 
+// new message in chat box
 function renderMessage(message,author){
-	let messageBody = document.createElement("p");
-	document.body.children[1].appendChild(messageBody);
-
-	messageBody.setAttribute("class","message");
-	if (author === username) {
+    // create message div
+    const messageBody = document.createElement("div");
+	// add to chat box
+    $("#chat-box").append(messageBody);
+    messageBody.setAttribute("class","message");
+	/* if user is author of message 
+     * render to right side in dark theme
+     * else to left with white theme
+     */
+    if (author === username) {
         messageBody.innerHTML = message + " | " + author;
     	messageBody.style.right = '10px';
     }
@@ -119,54 +108,128 @@ function renderMessage(message,author){
         messageBody.innerHTML = author + " | " + message;
         messageBody.style.background = "#fefefe";
         messageBody.style.color = "#242633";
-    	messageBody.style.left = '11%';
     }
-	
-	messageBody.style.top = rect+"px";
-	let p = $(".message");
-	let position = p.position();
+	// render to top with specific distance
+	messageBody.style.top = rect + "px";
+	const p = $(".message");
+	const position = p.position();
+    /* add distance for future message 
+     * to render lower 
+     */
 	rect += position.top + 50;
 }
+// get user rooms list
+function getRooms(){
+    chatSocket.send(JSON.stringify({
+        'command': 'get_rooms',
+        'email': accountEmail
+    }))
+}
 
+// get messages from selected room
+function fetchMessages(room){
+    chatSocket.send(JSON.stringify({
+        'command': 'fetch_messages',
+        'roomID': room
+    }));
+}
+// handle incoming room messages
+function handleFetchMessages(messages){
+    for (m in messages) {
+        const message = messages[m]
+        renderMessage(message['content'],message['author']);
+    }
+}
+
+// check if object is null
 function isEmptyObject(obj){
     return (Object.getOwnPropertyNames(obj).length === 0);
 }
 
-function closeRoomDialog() {
+function closeDialog() {
     members = [];
     roomDialog.style.display = "none";
 }
-
-function addMember(){
-    let email = document.getElementById('user-email-field').value;
-
-    if (email != null) {
+// add member to list for new room
+function memberToList(data){
+    if (data['name'] != username) {
+        // add to list member email
+        members.push(data['email']);
+        // update members number in dialog
+        $("#members").html("Members : " + members.length);
+        // empty email field
+        $("#user-email-field").val ('');
+    }else {
+        roomFormError("You can't add yourself");
+    }
+}
+// search if new member exist in database
+function searchMember(){
+    // email field text
+    const email = document.getElementById('user-email-field').value;
+    if (email != null && !members.includes(email)) {
         chatSocket.send(JSON.stringify({
             'command': 'search_user',
             'email': email
         }));
+    }else if(members.includes(email)) {
+        roomFormError("Member already added");
+    }else {
+        roomFormError("Member field empty");
     }
-}
-function createRoom(){
-    membersJson = {}
-    for (let i = 0; i < members.length;i++) {
-        let user = 'member' + i;
-        membersJson[user] = members[i];
-    }
-    createRoomRequest(membersJson);
 }
 
-function createRoomRequest(membersJson){
-    dataJson = {
-        'command': 'create_room',
-        'roomName': roomNameField.value,
-        'admin': username,
-        'members': membersJson
+function createRoom(){
+    if (!members.includes(accountEmail)) {
+        members.push(accountEmail);    
     }
     if (roomNameField.value === "") {
-        errorField.style.display = "block";
-        errorField.innerHTML = "Room name cannot be empty";
+        roomFormError("Room name cannot be empty");
     }else {
-        chatSocket.send(JSON.stringify(dataJson));
+        chatSocket.send(JSON.stringify({
+            'command': 'create_room',
+            'roomName': roomNameField.value,
+            'admin': accountEmail,
+            'members': members
+        }));
+    }
+}
+
+// handle all socket incoming data
+function responseHandler(response){
+    switch (response['response']) {
+        // add to list member for new room 
+        case 'Member exist':
+            memberToList(response);
+            break;
+        case 'User with that email does not exist':
+            roomFormError(response['response']);
+            break;
+        case 'Room already exist':
+            roomFormError(response['response']);
+            break;
+        // close dialog and get rooms
+        case 'Room created':
+            closeRoomDialog();
+            getRooms();
+            break;
+        case 'Message added':
+            break;
+        // get user rooms data
+        case 'User rooms':
+            renderRoomsList(response);
+            break;
+        // handle incoming messages when open room
+        case 'Messages':
+            handleFetchMessages(response['messages']);
+            break;
+        // render message in chat
+        default:
+            let message = response['message'];
+            renderMessage(
+                message['content'],
+                message['author']
+            )
+            break;
     }
 }
